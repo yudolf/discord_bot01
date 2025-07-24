76d7128aa68d386efa439f915086cd1c13cbb297
松本純 <jun@jmba2024.local>
2025-07-24 16:57:05 +0900

    Initial commit: Discord bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user}としてログインしました！')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    await message.channel.send('こんにちは！')
+    
+    await bot.process_commands(message)
+
+if __name__ == '__main__':
+    token = os.getenv('DISCORD_TOKEN')
+    if token:
+        bot.run(token)
+    else:
+        print('DISCORD_TOKENが設定されていません。.envファイルを確認してください。')
